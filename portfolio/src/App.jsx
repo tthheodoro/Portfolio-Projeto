66b3048ae2cd6b6330a4a1c0ae2cd89be6a06285
Tiago  Teodoro <119240603+tthheodoro@users.noreply.github.com>
2025-10-19 22:03:59 +0100

    Header

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import './style.css';
+import './_variables.css';
+
+
+function App() {
+  return (
+      <>
+        <Header> </Header>
+      </>
+  );
+}
+
+export default App
