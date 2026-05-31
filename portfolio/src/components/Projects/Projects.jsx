91979999f6207873f8601c1a238e413d8098c4ec
Tiago <tiagocteodoro@email.com>
2026-05-31 18:24:40 +0100

    Update Projects.jsx

---
+++ portfolio/src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// ... os teus outros imports
+
+function Projects() {
+    return (
+        <section>
+            {/* o código do teu componente */}
+        </section>
+    );
+}
+
+export default Projects; // <-- É ESTA A LINHA QUE ESTÁ A FALTAR!
