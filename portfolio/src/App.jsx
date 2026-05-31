649077c090b5c991286683d16ac6e299d7c6af48
Tiago <tiagocteodoro@email.com>
2026-05-31 18:21:51 +0100

    .

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,25 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Principal from "./components/Principal/Principal";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 import './style.css';
 import './_variables.css';
 
-
 function App() {
   return (
-      <>
-        <Header> </Header>
-        <Principal></Principal>
-        <Footer></Footer>
-      </>
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Principal />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+
+      <Footer />
+    </HashRouter>
   );
 }
 
-export default App
+export default App;
