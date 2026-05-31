6962f78f315c951e5f2a0ebf772f6a17e4f05184
Tiago <tiagocteodoro@email.com>
2026-05-31 16:08:33 +0100

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,7 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio-Projeto/',
-  plugins: [react(),tailwindcss()],
-  
+  plugins: [react(), tailwindcss()],
 })
