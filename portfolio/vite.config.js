ce3312072c2b0f0ef07d6cf8b37117ad1bd8255b
Tiago <tiagocteodoro@email.com>
2026-06-06 23:14:32 +0100

    .

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio-Projeto/',
   plugins: [react(), tailwindcss()],
   resolve: {
     dedupe: ['react', 'react-dom']
