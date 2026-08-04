cb5e18da8e873f49f1ae0c409214eb418a9b7a25
Tiago <tiagocteodoro@email.com>
2026-08-04 12:27:48 +0100

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,23 +1,15 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import Principal from "./components/Principal/Principal";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import './style.css';
-import './_variables.css';
+
+import "./style.css";
+import "./_variables.css";
 
 function App() {
   return (
     <HashRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Principal />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
       </Routes>
-
-      <Footer />
     </HashRouter>
   );
 }
